71fd02d1741addd512f14eefacc55f5a61272eb1
Sun Lin <sunlin@mozat.com>
2018-05-30 13:52:04 +0800

    Update archive configuration

--- ios/eoswallet/AppDelegate.m
+++ ios/eoswallet/AppDelegate.m
@@ -16,8 +16,12 @@
 {
   NSURL *jsCodeLocation;
 
-  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"eoswallet"
