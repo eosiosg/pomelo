ceb5a68866562049f57486d0daf270ade5771278
test <test@test.com>
2018-05-31 13:45:44 +0800

    fix bug

--- android/app/src/main/java/com/eoswallet/MainApplication.java
+++ android/app/src/main/java/com/eoswallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.eoswallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
