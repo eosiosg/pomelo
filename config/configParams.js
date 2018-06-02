249de9aac9517489643316a87f5c1a3898188ad5
Sun Lin <sunlin@mozat.com>
2018-06-02 20:03:00 +0800

    Add mainnet configuration

--- config/configParams.js
+++ config/configParams.js
@@ -3,10 +3,23 @@
  */
 
 export const versionNumber = '1.0';
+
+
+export const chainId = '706a7ddd808de9fc2b8879904f3b392256c83104c1d544b38302cc07d9fca477';
+export const updatingDetectURL = 'https://api.eosio.sg/upgrade';
+export const defaultLogoUrl = 'https://steemitimages.com/0x0/https://steemitimages.com/DQmWs93EuTs1CrgazwChGBqj7eygJoQ4HqB9ANWMN8TMHc8/eoosss1920_0023.jpg';
+
+
+//test env
 export const nodeAddressList = [
     'http://13.229.70.163:8888',
     'http://52.74.197.107:8888',
 ];
-export const chainId = '706a7ddd808de9fc2b8879904f3b392256c83104c1d544b38302cc07d9fca477';
-export const updatingDetectURL = 'https://api.eosio.sg/upgrade';
-export const defaultLogoUrl = 'https://steemitimages.com/0x0/https://steemitimages.com/DQmWs93EuTs1CrgazwChGBqj7eygJoQ4HqB9ANWMN8TMHc8/eoosss1920_0023.jpg';
+
+
+//mainnet env
+// export const nodeAddressList = [
+//     'https://mainnet.eosio.sg',
+//     'https://mainnet.eoscannon.io',
+//     'https://mainnet.meet.one'
+// ];
