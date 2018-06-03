8de34d976efbf5f1b30c96236dd4c86e9e110193
Sun Lin <sunlin@mozat.com>
2018-06-03 09:03:47 +0800

    update build scripts

--- clean_run.sh
+++
@@ -1,7 +0,0 @@
-
-rm -rf node_modules
-rm -rf /ios/build
-rm -rf /tmp/metro-bundler-cache-*
-rm -rf /tmp/haste-map-react-native-packager-*
-yarn install2
-react-native run-ios  --simulator="iPhone 8"
